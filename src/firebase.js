08ab605944c2830589ec54784c634e941313265f
Vector <114653983+pageyohan@users.noreply.github.com>
2025-03-02 10:12:14 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAgV9ud9-5kDs62Z7ELjExabdEu7nK_jr8",
-    authDomain: "gif-51a33.firebaseapp.com",
-    projectId: "gif-51a33",
-    storageBucket: "gif-51a33.firebasestorage.app",
-    messagingSenderId: "875853543599",
-    appId: "1:875853543599:web:3e9aee69d31939b81e3c5a"
-  };
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
+};
 
 
 // Initialiser Firebase
